bf4fe27c6999805adae8f75ddd6cc0f489305a6d
xkraty <kry619@gmail.com>
2023-06-19 19:40:54 +0100

    disabled open tok

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,8 +3,8 @@ class Room < ApplicationRecord
   has_one :chatroom, dependent: :destroy
 
   before_create do
-    opentok = OpenTok::OpenTok.new ENV["VONAGE_API_KEY"], ENV["VONAGE_API_SECRET"]
-    session = opentok.create_session
-    self.vonage_session_id = session.session_id
+    # opentok = OpenTok::OpenTok.new ENV["VONAGE_API_KEY"], ENV["VONAGE_API_SECRET"]
+    # session = opentok.create_session
+    # self.vonage_session_id = session.session_id
   end
 end
